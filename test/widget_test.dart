a3f711d89e78169ddba6f0e4f4ca4ef87a00562d
chiragdhunna <chiragdhunna2468@gmail.com>
2025-04-09 18:42:28 +0530

    correcting test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pizza_striker/main.dart';
+import 'package:pizza_striker/services/theme_service.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final isDarkMode = await ThemeService.loadThemeFromPrefs();
+    await tester.pumpWidget(MyApp(isDarkMode: isDarkMode));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
